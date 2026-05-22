99223eb8009c2c35b2d7479cbf2b4b6a1ee4b843
rodrigodrocha <rodrigo.rocha9229@gmail.com>
2026-05-22 14:21:18 -0300

    Usando o FileWriter e o FileReader para arquivos

---
+++ 00_Introducao_ao_Java_basico_ao_avancado/10_I-O/01_Lendo_e_escrevendo/Exemplo04_FileReaderWriter.java
@@ -0,0 +1,17 @@
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class Exemplo04_FileReaderWriter {
+    public static void main(String[] args) throws IOException{
+       
+       
+        try (FileWriter out = new FileWriter("pastinha/out1.txt", StandardCharsets.UTF_8)){
+            out.write("Hello!");
+
+       }
+
+    }
+
+}
